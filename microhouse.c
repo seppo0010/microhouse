de11e60692d717bd52496b39b6a295089a056b99
Sebastian Waisbrot <seppo0010@gmail.com>
2011-03-07 16:56:43 -0300

    Add is_php function

--- microhouse.c
+++ microhouse.c
@@ -7,9 +7,25 @@
 #include "benchmark.h"
 #include "controller.h"
 
+#include "ext/standard/php_versioning.h"
+
 static microhouse_singletons *singletons = NULL;;
 static function_entry microhouse_functions[] = {
 	PHP_FE(microhouse_version, NULL)
+	PHP_FE(is_php, NULL)
+/*
+	PHP_FE(is_really_writable, NULL)
+	PHP_FE(load_class, NULL)
+	PHP_FE(is_loaded, NULL)
+	PHP_FE(get_config, NULL)
+	PHP_FE(config_item, NULL)
+	PHP_FE(show_error, NULL)
+	PHP_FE(show_404, NULL)
+	PHP_FE(log_message, NULL)
+	PHP_FE(set_status_header, NULL)
+	PHP_FE(_exception_handler, NULL)
+	PHP_FE(remove_invisible_characters, NULL)
+*/
 	{NULL, NULL, NULL}
 };
 
@@ -20,7 +36,7 @@ zend_module_entry microhouse_module_entry = {
 	PHP_MICROHOUSE_EXTNAME,
 	microhouse_functions,
 	PHP_MINIT(microhouse),
-	NULL,
+	PHP_MSHUTDOWN(microhouse),
 	PHP_RINIT(microhouse),
 	PHP_RSHUTDOWN(microhouse),
 	NULL,
@@ -34,10 +50,22 @@ zend_module_entry microhouse_module_entry = {
 ZEND_GET_MODULE(microhouse)
 #endif
 
+static HashTable _is_php;
 PHP_MINIT_FUNCTION(microhouse)
 {
 	mhbenchmark_init(TSRMLS_C);
 	mhcontroller_init(TSRMLS_C);
+
+	zend_hash_init(&_is_php, 4, NULL, NULL, 1);
+
+	return SUCCESS;
+}
+
+PHP_MSHUTDOWN_FUNCTION(microhouse)
+{
+	HashTable *ht = &_is_php;
+	zend_hash_destroy(ht);
+
 	return SUCCESS;
 }
 
@@ -78,4 +106,29 @@ void microhouse_set_controller(void *controller TSRMLS_DC)
 PHP_FUNCTION(microhouse_version)
 {
 	RETURN_STRING(PHP_MICROHOUSE_VERSION, 1);
+}
+
+PHP_FUNCTION(is_php)
+{
+	char *version = NULL;
+	int version_len;
+
+	if (zend_parse_parameters(ZEND_NUM_ARGS() TSRMLS_CC, "|s", &version, &version_len) == FAILURE) {
+		return;
+	}
+
+	if (version == NULL) {
+		version = "5.0.0";
+		version_len = sizeof(version);
+	}
+
+        if (zend_hash_exists(&_is_php, version, version_len + 1)) {
+		int *ret;
+		zend_hash_find(&_is_php, version, version_len + 1, (void **) &ret);
+		RETURN_BOOL(*ret);
+        }
+
+	int result = php_version_compare(PHP_VERSION, version) >= 0;
+	zend_hash_add(&_is_php, version, version_len + 1, &result, sizeof(int*), NULL);
+	RETURN_BOOL(result);
 }
--- php_microhouse.h
+++ php_microhouse.h
@@ -5,10 +5,28 @@
 #define PHP_MICROHOUSE_EXTNAME "MicroHouse"
 
 PHP_MINIT_FUNCTION(microhouse);
+PHP_MSHUTDOWN_FUNCTION(microhouse);
 PHP_RINIT_FUNCTION(microhouse);
 PHP_RSHUTDOWN_FUNCTION(microhouse);
 
 PHP_FUNCTION(microhouse_version);
+
+/* Common */
+PHP_FUNCTION(is_php);
+/*
+PHP_FUNCTION(is_really_writable);
+PHP_FUNCTION(load_class);
+PHP_FUNCTION(is_loaded);
+PHP_FUNCTION(get_config);
+PHP_FUNCTION(config_item);
+PHP_FUNCTION(show_error);
+PHP_FUNCTION(show_404);
+PHP_FUNCTION(log_message);
+PHP_FUNCTION(set_status_header);
+PHP_FUNCTION(_exception_handler);
+PHP_FUNCTION(remove_invisible_characters);
+*/
+/* End Common */
 
 typedef struct microhouse_singletons {
 	void *controller;
