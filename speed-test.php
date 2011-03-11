4e7400002db56fe2f273303cb90623f05fa0fbc9
Sebastian Waisbrot <seppo0010@gmail.com>
2011-03-11 20:27:56 -0300

    Change speed-test parameters to allow more flexibility

--- speed-test.php
+++ speed-test.php
@@ -1,12 +1,17 @@
 <?php
 $time = 0.0;
 $count = 0;
-for ($i = 0; $i < 1000; ++$i) {
-	$contents = file_get_contents('http://localhost:8080/' . $argv[1] . '/index.php');
+$times = 1000;
+if ($argc > 2) $times = $argv[2];
+$port = 80;
+if ($argc > 3) $port = $argv[3];
+
+for ($i = 0; $i < $times; ++$i) {
+	$contents = file_get_contents('http://localhost:' . $port . '/' . $argv[1] . '/index.php');
 	$matches = NULL;
-echo "$i\n";
 	if (0 < preg_match('/Page rendered in ([0-9]+\.[0-9]+) seconds/', $contents, $matches)) {
 		$time += (float)$matches[1];
 		++$count;
 	}
 }
+echo number_format($time / $count, 6) , "\n";
